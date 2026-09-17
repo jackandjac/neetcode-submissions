bfd8ba6869ae7d6279035a2052c907382561a84d
jackandjac <jackandjac@gmail.com>
2026-09-16 21:44:11 -0500

    Add: meeting-schedule - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule/submission-3.java
@@ -0,0 +1,29 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        if (intervals.size() <= 1) {
+            return true;
+        }
+        intervals.sort((a, b)-> a.start - b.start);
+        int curEnd = intervals.get(0).end;
+
+        for (int i = 1; i < intervals.size(); i++) {
+            if (curEnd > intervals.get(i).start) {
+                return false;
+            } else {
+                curEnd = intervals.get(i).end;
+            }
+        }
+        return true;
+    }
+}
